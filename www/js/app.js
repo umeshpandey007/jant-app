f99c21abd80beb232c05b65a7328d984ccb353de
umeshpandey007 <umeshpandey007@gmail.com>
2015-09-13 00:23:15 -0700

    Basic UI framework is up

--- www/js/app.js
+++ www/js/app.js
@@ -3,17 +3,43 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+(function IIFE(window) {
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
+  var jantApp;
+
+  jantApp = angular.module('jantApp', ['ionic']);
+
+  jantApp.config(['$stateProvider','$urlRouterProvider',function($stateProvider,$urlRouterProvider){
+    $stateProvider
+      .state('topics',{
+        url: '/topics',
+        templateUrl: 'partials/topics.html',
+        //controller: 'TopicCtrl'
+      })
+      .state('notes',{
+        url: '/notes',
+        templateUrl: 'partials/notes.html',
+        //controller: 'NoteCtrl'
+      })
+      .state('edit',{
+        url:'/edit',
+        templateUrl: 'partials/edit.html',
+        //controller: 'EditCtrl'
+      });
+
+    $urlRouterProvider.otherwise('/topics');
+  }]);
+
+  jantApp.run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if(window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      }
+      if(window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+    });
   });
-})
+})(window);
